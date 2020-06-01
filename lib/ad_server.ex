f09432998de14d91150194141cbb734141be6c94
Pablo Gomez <pablonahuelgomez@gmail.com>
2020-06-01 17:32:33 -0300

    Flat map results

--- lib/ad_server.ex
+++ lib/ad_server.ex
@@ -40,7 +40,7 @@ defmodule QCEC.AdServer do
   @impl true
   def handle_call(:all, _from, state) do
     ads = QCEC.Categories.list(:names)
-    |> Enum.map(fn category_name -> QCEC.AdCacheServer.lookup(category_name) end)
+    |> Enum.flat_map(fn category_name -> QCEC.AdCacheServer.lookup(category_name) end)
     {:reply, ads, state}
   end
 
