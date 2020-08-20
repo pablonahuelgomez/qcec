7b2f0be6ad7cc3cb977493731761719ae5688e50
Pablo Gomez <pablonahuelgomez@gmail.com>
2020-08-20 15:38:18 -0300

    Add Categories. Update impl details to share trans

--- lib/util/categories.ex
+++ lib/util/categories.ex
@@ -1,37 +1,59 @@
 defmodule QCEC.Categories do
   @categories [
-    {:bakery, 1},
-    {:sushi, 2},
-    {:processed_meals, 3},
-    {:pet_food, 4},
-    {:ice_creams, 5},
-    {:warehouse_products, 7},
-    {:cleaning_products, 8},
-    {:fruits_and_vegetables, 9},
-    {:meat_and_fish, 10},
-    {:dietetics, 11},
-    {:pizzas_and_empanadas, 12},
-    {:spare_parts_for_vehicles, 15},
-    {:hardware_stores, 16},
-    {:clothes_washers, 17},
-    {:pharmacies, 18},
-    {:bookstore_items, 19},
-    {:markets, 20},
-    {:bookstores, 21},
-    {:clothing_and_related, 22},
-    {:footwear, 23},
-    {:paint_shops, 24},
-    {:leather_goods, 25},
-    {:toy_stores, 26},
-    {:drinks, 27},
-    {:pasta, 28},
-    {:without_tacc, 29},
-    {:bazaar_and_decoration, 30},
-    {:optics, 31}
+    {:bakery, 1, "panadería"},
+    {:sushi, 2, "sushi"},
+    {:processed_meals, 3, "comidas"},
+    {:pet_food, 4, "alimentos para mascotas"},
+    {:ice_creams, 5, "heladerías"},
+    {:warehouse_products, 7, "productos de almacen"},
+    {:cleaning_products, 8, "productos de limpieza"},
+    {:fruits_and_vegetables, 9, "frutas y verduras"},
+    {:meat_and_fish, 10, "carnes y pescados"},
+    {:dietetics, 11, "dietéticas"},
+    {:pizzas_and_empanadas, 12, "pizzas & empanadas"},
+    {:spare_parts_for_vehicles, 15, "repuestos para vehículos"},
+    {:hardware_stores, 16, "ferreterías"},
+    {:clothes_washers, 17, "lavaderos de ropa"},
+    {:pharmacies, 18, "farmacias"},
+    {:bookstore_items, 19, "artículos de librería"},
+    {:markets, 20, "mercados"},
+    {:bookstores, 21, "librerías"},
+    {:clothing_and_related, 22, "indumentaria"},
+    {:footwear, 23, "calzado"},
+    {:paint_shops, 24, "pinturerías"},
+    {:leather_goods, 25, "marroquinerías"},
+    {:toy_stores, 26, "jugueterías"},
+    {:drinks, 27, "bebidas"},
+    {:pasta, 28, "pastas"},
+    {:without_tacc, 29, "sin tacc"},
+    {:bazaar_and_decoration, 30, "decoración y bazaar"},
+    {:optics, 31, "óptica"},
+    {:baby_children_clothing, 32, "indumentaria bebes / niños"},
+    {:sports, 33, "artículos deportivos"},
+    {:cellphones_informatics, 34, "celulares / informática"},
+    {:blankets, 35, "blanquería"},
+    {:lingerie, 36, "lencería"},
+    {:merceries, 37, "mercerías"},
+    {:music, 38, "música"},
+    {:cotillon, 39, "cotillón"},
+    {:electrodomestics, 40, "electrodomésticos"},
+    {:jewelry, 41, "joyas / relojes"},
+    {:furniture, 42, "mueblerías"},
+    {:perfumery, 43, "perfumería"},
+    {:luminary, 44, "iluminación"},
+    {:plants, 45, "viveros"},
+    {:personal_hygiene, 46, "higiene personal"},
+    {:craft, 47, "artesanías / economía social"},
+    {:hair_products, 48, "insumos peluquerías"}
   ]
 
   def list(:all), do: @categories
-  def list(:names), do: @categories |> Enum.map(fn {name, _} -> name end)
+  def list(:names), do: @categories |> Enum.map(fn {name, _, _} -> name end)
 
-  def id(category_name), do: Keyword.get(@categories, category_name)
+  def id(category_name) do
+    {_, id, _} = Enum.find(@categories, fn {name, _, _} ->
+      name == category_name
+    end)
+    id
+  end
 end
